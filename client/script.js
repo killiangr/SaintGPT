469dd113243ff3abedc15a3902b6ab83c7d27d8c
killiangr <killian.graindorge@efrei.net>
2023-02-03 09:19:14 +0100

    loader

--- client/script.js
+++ client/script.js
@@ -1,1 +1,19 @@
+import bot from './assets/bot.svg';
+import user from './assets/user.svg';
 
+const form = document.querySelector('form');
+const chatContainer = document.querySelector('#chat_container');
+
+let laodInterval;
+
+function loader (element) {
+  element.textContent ='';
+  laodInterval = setInterval(() => {
+    element.textContent += '.';
+
+    if (element.textContent === '....') {
+      element.textContent = '';
+    }
+  }, 300)
+
+}
